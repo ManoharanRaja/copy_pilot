c37ae107cd192503b10eb04e7fce8d0b19d01fbd
Mojo <mojo@gmail.com>
2025-07-03 02:03:04 +0100

    Datasource newlook added

--- frontend/src/pages/datasource/DataSource.js
+++ frontend/src/pages/datasource/DataSource.js
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
-  Container,
   Typography,
   Button,
-  Paper,
   Table,
   TableBody,
   TableCell,
@@ -41,80 +39,86 @@ function DataSource() {
   };
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 4 }}>
-      <Paper elevation={3} sx={{ p: 4 }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            mb: 3,
-          }}
+    <Box
+      sx={{
+        width: "100vw",
+        minHeight: "100vh",
+        bgcolor: "transparent",
+        px: { xs: 2, md: 6 },
+        py: 4,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          mb: 3,
+        }}
+      >
+        <Typography variant="h4">Data Sources</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/datasource/new")}
         >
-          <Typography variant="h4">Data Sources</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => navigate("/datasource/new")}
-          >
-            Add New Data Source
-          </Button>
-        </Box>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Type</TableCell>
-                <TableCell>Config</TableCell>
-                <TableCell>Actions</TableCell>
+          Add New Data Source
+        </Button>
+      </Box>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Config</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sources.map((src) => (
+              <TableRow key={src.id}>
+                <TableCell>{src.name}</TableCell>
+                <TableCell>{src.type}</TableCell>
+                <TableCell>
+                  {src.type === "Azure Data Lake Storage"
+                    ? `Account: ${src.config.account_name}, Container: ${src.config.container}`
+                    : src.type === "local"
+                    ? `Path: ${src.config.path}`
+                    : "-"}
+                </TableCell>
+                <TableCell>
+                  <Stack direction="row" spacing={1}>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      onClick={() => handleEditPage(src.id)}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      size="small"
+                      onClick={() => handleDelete(src.id)}
+                    >
+                      Delete
+                    </Button>
+                  </Stack>
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {sources.map((src) => (
-                <TableRow key={src.id}>
-                  <TableCell>{src.name}</TableCell>
-                  <TableCell>{src.type}</TableCell>
-                  <TableCell>
-                    {src.type === "Azure Data Lake Storage"
-                      ? `Account: ${src.config.account_name}, Container: ${src.config.container}`
-                      : src.type === "local"
-                      ? `Path: ${src.config.path}`
-                      : "-"}
-                  </TableCell>
-                  <TableCell>
-                    <Stack direction="row" spacing={1}>
-                      <Button
-                        variant="outlined"
-                        size="small"
-                        onClick={() => handleEditPage(src.id)}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        variant="outlined"
-                        color="error"
-                        size="small"
-                        onClick={() => handleDelete(src.id)}
-                      >
-                        Delete
-                      </Button>
-                    </Stack>
-                  </TableCell>
-                </TableRow>
-              ))}
-              {sources.length === 0 && (
-                <TableRow>
-                  <TableCell colSpan={4} align="center">
-                    No data sources found.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Container>
+            ))}
+            {sources.length === 0 && (
+              <TableRow>
+                <TableCell colSpan={4} align="center">
+                  No data sources found.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
